38d611b0e85363cb7bc6d9eeef1286aebc9afa04
Alex-2004-04 <alexandrocapuano04@gmail.com>
2026-01-28 13:24:04 +0100

    aggiornamento navbar pagina account

--- components/account/Navbar.tsx
+++ components/account/Navbar.tsx
@@ -2,62 +2,52 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 
-const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // URL dell'avatar estratto dal tuo snippet
-  const avatarUrl = "https://occ-0-2582-2581.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXh10ggeTTdhZO1JIH_SNQ4gp0vsNnWfE8Mg2ckwzGvUzJMRpPFCujRK3Ex5K9VbkIyvUHQ92LBVdsemkj6zlpquL-qWMCNKeg.png?r=229";
-
+const AccountNavbar = () => {
   return (
-    <header className="fixed top-0 w-full z-50 bg-black/90 px-4 py-2 md:px-10 flex items-center justify-between transition-all duration-300">
-      {/* --- Logo Section --- */}
-      <div className="flex items-center">
-        <Link href="/browse" aria-label="Netflix Home">
-          <svg
-            viewBox="0 0 111 30"
-            className="w-24 h-auto text-red-600 fill-current"
-            aria-hidden="true"
-            role="img"
-          >
-            <g>
-              <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z"></path>
-            </g>
-          </svg>
-        </Link>
-      </div>
-
-      {/* --- User Menu Section --- */}
-      <div className="flex items-center gap-4">
-        <button 
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="flex items-center gap-2 cursor-pointer group focus:outline-none"
-          aria-expanded={isMenuOpen}
-          aria-haspopup="true"
-        >
-          {/* Avatar Image */}
-          <div className="relative w-8 h-8 rounded-sm overflow-hidden">
-             <Image 
-                src={avatarUrl}
-                alt="Profilo Utente"
-                width={32}
-                height={32}
-                className="object-cover"
-                unoptimized // Usato perché è un URL esterno diretto
-             />
-          </div>
-          
-          {/* Caret Icon - Ruota quando attivo */}
-          <span className={`text-white transition-transform duration-200 ${isMenuOpen ? 'rotate-180' : ''}`}>
-            <svg viewBox="0 0 16 16" width="16" height="16" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M11.6 6.5c.15 0 .22.18.12.28l-3.48 3.48a.33.33 0 0 1-.48 0L4.28 6.78a.17.17 0 0 1 .12-.28z" clipRule="evenodd"></path>
+    <header className="w-full bg-white border-b border-gray-200 shadow-sm">
+      {/* Usiamo un container interno con max-width per centrare gli elementi 
+          e distanziarli dai bordi proprio come nell'immagine originale 
+      */}
+      <div className="max-w-[1200px] mx-auto flex items-center justify-between px-8 py-4">
+        
+        {/* Logo Netflix - Leggermente più grande e distanziato */}
+        <div className="flex items-center">
+          <Link href="/browse">
+            <svg
+              viewBox="0 0 111 30"
+              className="w-28 md:w-32 h-auto fill-[#E50914] transition-opacity hover:opacity-80"
+              aria-hidden="true"
+            >
+              <path d="M105.06233,14.2806261 L110.999156,30 C109.249227,29.7497422 107.500234,29.4366857 105.718437,29.1554972 L102.374168,20.4686475 L98.9371075,28.4375293 C97.2499766,28.1563408 95.5928391,28.061674 93.9057081,27.8432843 L99.9372012,14.0931671 L94.4680851,-5.68434189e-14 L99.5313525,-5.68434189e-14 L102.593495,7.87421502 L105.874965,-5.68434189e-14 L110.999156,-5.68434189e-14 L105.06233,14.2806261 Z M90.4686475,-5.68434189e-14 L85.8749649,-5.68434189e-14 L85.8749649,27.2499766 C87.3746368,27.3437061 88.9371075,27.4055675 90.4686475,27.5930265 L90.4686475,-5.68434189e-14 Z M81.9055207,26.93692 C77.7186241,26.6557316 73.5307901,26.4064111 69.250164,26.3117443 L69.250164,-5.68434189e-14 L73.9366389,-5.68434189e-14 L73.9366389,21.8745899 C76.6248008,21.9373887 79.3120255,22.1557784 81.9055207,22.2804387 L81.9055207,26.93692 Z M64.2496954,10.6561065 L64.2496954,15.3435186 L57.8442216,15.3435186 L57.8442216,25.9996251 L53.2186709,25.9996251 L53.2186709,-5.68434189e-14 L66.3436123,-5.68434189e-14 L66.3436123,4.68741213 L57.8442216,4.68741213 L57.8442216,10.6561065 L64.2496954,10.6561065 Z M45.3435186,4.68741213 L45.3435186,26.2498828 C43.7810479,26.2498828 42.1876465,26.2498828 40.6561065,26.3117443 L40.6561065,4.68741213 L35.8121661,4.68741213 L35.8121661,-5.68434189e-14 L50.2183897,-5.68434189e-14 L50.2183897,4.68741213 L45.3435186,4.68741213 Z M30.749836,15.5928391 C28.687787,15.5928391 26.2498828,15.5928391 24.4999531,15.6875059 L24.4999531,22.6562939 C27.2499766,22.4678976 30,22.2495079 32.7809542,22.1557784 L32.7809542,26.6557316 L19.812541,27.6876933 L19.812541,-5.68434189e-14 L32.7809542,-5.68434189e-14 L32.7809542,4.68741213 L24.4999531,4.68741213 L24.4999531,10.9991564 C26.3126816,10.9991564 29.0936358,10.9054269 30.749836,10.9054269 L30.749836,15.5928391 Z M4.78114163,12.9684132 L4.78114163,29.3429562 C3.09401069,29.5313525 1.59340144,29.7497422 0,30 L0,-5.68434189e-14 L4.4690224,-5.68434189e-14 L10.562377,17.0315868 L10.562377,-5.68434189e-14 L15.2497891,-5.68434189e-14 L15.2497891,28.061674 C13.5935889,28.3437998 11.906458,28.4375293 10.1246602,28.6868498 L4.78114163,12.9684132 Z" />
             </svg>
-          </span>
-        </button>
+          </Link>
+        </div>
+
+        {/* Sezione Destra: Icona Profilo */}
+        <div className="flex items-center">
+          <div className="flex items-center gap-1 border border-gray-300 rounded p-1 pr-2 hover:bg-gray-100 cursor-pointer transition-colors">
+            <div className="w-8 h-8 relative rounded overflow-hidden">
+              <Image
+                src="https://occ-0-2582-2581.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXh10ggeTTdhZO1JIH_SNQ4gp0vsNnWfE8Mg2ckwzGvUzJMRpPFCujRK3Ex5K9VbkIyvUHQ92LBVdsemkj6zlpquL-qWMCNKeg.png?r=229"
+                alt="Avatar"
+                fill
+                className="object-cover"
+                unoptimized
+              />
+            </div>
+            <svg
+              viewBox="0 0 24 24"
+              className="w-5 h-5 text-gray-500"
+              fill="currentColor"
+            >
+              <path d="M7 10l5 5 5-5z" />
+            </svg>
+          </div>
+        </div>
       </div>
     </header>
   );
 };
 
-export default Navbar;
+export default AccountNavbar;
